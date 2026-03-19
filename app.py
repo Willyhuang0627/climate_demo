ed47e27b6c28ae44d50add5c8568eb3d93c4fe81
willyhuang <aa0981273968@gmail.com>
2026-03-19 13:40:31 +0800

    1st update

---
+++ app.py
@@ -0,0 +1,19 @@
+from datetime import date
+import matplotlib.pyplot as plt
+import meteostat as ms
+
+# Specify location and time range
+POINT = ms.Point(25.0354294,121.5334188)  # Try with your location
+START = date(2022, 1, 1)
+END = date(2022, 12, 31)
+
+# Get nearby weather stations
+stations = ms.stations.nearby(POINT, limit=4)
+
+# Get daily data & perform interpolation
+ts = ms.daily(stations, START, END)
+df = ms.interpolate(ts, POINT).fetch()
+df.to_csv("data.csv")  # Save data to CSV file
+# Plot line chart including average, minimum and maximum temperature
+df.plot(y=[ms.Parameter.TEMP, ms.Parameter.TMIN, ms.Parameter.TMAX])
+plt.show()
---
+++ sta_list.py
@@ -0,0 +1,15 @@
+from datetime import date
+import matplotlib.pyplot as plt
+import meteostat as ms
+
+
+# 建立座標
+loc = ms.Point(24.19352298812673, 120.66940049145083)
+sv = ms.Point(37.39322970859948, -121.5466194773837)
+
+slist = ms.stations.nearby(sv,limit=3)
+slist2 = ms.stations.nearby(loc,limit=3)
+
+print("在指定座標附近的偵測點如下: ")
+print( slist )
+print( slist2 )
